0686adb57d21e1b4de6054d6aa34e838422a8a16
Himanshu Joshi <himanshu.joshi@moengage.com>
2024-04-05 11:56:25 +0530

    Initial moengage notification center setup

--- app/src/main/java/com/example/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/MainActivity.kt
@@ -32,6 +32,7 @@ import com.example.newsapp.util.Utils
 import com.moengage.core.Properties
 import com.moengage.core.analytics.MoEAnalyticsHelper
 import com.moengage.core.model.AppStatus
+import com.moengage.inbox.ui.view.InboxActivity
 import com.moengage.pushbase.MoEPushHelper
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -316,9 +317,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener, NewsA
      */
 
     override fun onItemClick(position: Int, articles: List<Articles>) {
-        val url = articles[position].url
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.setData(Uri.parse(url))
+        val intent = Intent(this, InboxActivity::class.java)
         startActivity(intent)
     }
 }
